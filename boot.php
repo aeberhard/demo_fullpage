31d40d8b8e4e5649e824f6f17550a53c6ccce659
aeberhard <andreaseberhard@gmail.com>
2018-02-07 01:16:29 +0100

    Anpassung boot.php
    rex_developer_manager::setBasePath nur wenn developer-Addon auch wirklich vorhanden ist

--- boot.php
+++ boot.php
@@ -2,7 +2,7 @@
 
 // --- NO_DIST
 // developer-Addon - Module und Templates ins Addon-Verzeichnis synchronisieren
-if (rex_addon::get('developer')->isInstalled()) {
+if (rex_addon::get('developer')->isInstalled() && file_exists(rex_path::addon('developer')) && is_dir(rex_path::addon('developer'))) {
     rex_developer_manager::setBasePath(rex_path::addon('demo_fullpage'));
 }
 // --- /NO_DIST
